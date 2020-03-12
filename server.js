e57bc7884f2260636f53cd8fe08d719f6079dfdf
Evan Harrison <esharri2@gmail.com>
2020-03-11 21:15:13 -0400

    save point

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+);
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
