ce41c37f37b0886fd64b6a20ed3f6c09f0b4baab
Evan Harrison <esharri2@gmail.com>
2020-03-12 20:24:14 -0400

    add pass through for admin site

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,16 +6,16 @@ module.exports = config => {
   config.addLayoutAlias("default", "layouts/default.njk");
 
   // Include our static assets
+  config.addPassthroughCopy("site/admin");
   config.addPassthroughCopy("site/media");
   config.addPassthroughCopy({ "site/assets": "/" });
   config.setUseGitIgnore(false);
 
-
   // You can return your Config object (optional).
   return {
     templateFormats: ["md", "njk"],
-    markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
     passthroughFileCopy: true,
     dir: {
       input: "site",
