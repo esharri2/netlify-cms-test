cc22c8d9573320c97f622cc3fe35477be609b99b
Evan Harrison <esharri2@gmail.com>
2020-04-11 11:46:53 -0400

    add youtube component

--- js/admin/index.js
+++ js/admin/index.js
@@ -3,7 +3,7 @@ const pathname = new URL(window.location.href).pathname;
 // TODO tighten this condition
 if (pathname.includes("/admin")) {
   (async () => {
-      await import("loadNetlifyIdentity.js");
-      import("editorComponents");
+      await import("./loadNetlifyIdentity.js");
+      import("./editorComponents");
   })();
 }
