d5622408691d971fca9637ead980bf5723cf27a8
Evan Harrison <esharri2@gmail.com>
2020-04-11 11:59:57 -0400

    add youtube widget

--- js/admin/index.js
+++ js/admin/index.js
@@ -1,10 +1,7 @@
 const pathname = new URL(window.location.href).pathname;
-console.log("pathname is ", pathname);
 // TODO tighten this condition
 if (pathname.includes("/admin")) {
   (async () => {
-      console.log("Going to load admin scripts...");
       await import("./loadNetlifyIdentity.js");
-      import("./editorComponents");
   })();
 }
