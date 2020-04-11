db385435d9f4ba2a9ce5928bafbc0100bbd6133d
Evan Harrison <esharri2@gmail.com>
2020-04-11 11:54:10 -0400

    update logs

--- js/admin/index.js
+++ js/admin/index.js
@@ -1,8 +1,9 @@
 const pathname = new URL(window.location.href).pathname;
-
+console.log("pathname is ", pathname);
 // TODO tighten this condition
 if (pathname.includes("/admin")) {
   (async () => {
+      console.log("Going to load admin scripts...");
       await import("./loadNetlifyIdentity.js");
       import("./editorComponents");
   })();
--- js/admin/loadNetlifyIdentity.js
+++ js/admin/loadNetlifyIdentity.js
@@ -1,4 +1,5 @@
 (function(){
+  console.log("going to load netlify identity widget...");
   const newScript = document.createElement("script");
   newScript.src = "https://identity.netlify.com/v1/netlify-identity-widget.js";
   document.body.appendChild(newScript);
