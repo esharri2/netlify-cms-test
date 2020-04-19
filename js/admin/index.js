5e4e5f364bed34a936fa2baf2f6a6c5c1209ac46
Evan Harrison <esharri2@gmail.com>
2020-04-19 07:55:57 -0400

    test

--- js/admin/index.js
+++ js/admin/index.js
@@ -1,7 +1,12 @@
 const pathname = new URL(window.location.href).pathname;
 // TODO tighten this condition
-if (pathname.includes("/admin")) {
+// if (pathname.includes("/admin")) {
+//   (async () => {
+//       await import("./loadNetlifyIdentity.js");
+//   })();
+// }
+
+
   (async () => {
-      await import("./loadNetlifyIdentity.js");
+    await import("./loadNetlifyIdentity.js");
   })();
-}
