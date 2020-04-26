21783aed87937574b5bdc3d9db662beebdc80d50
Evan Harrison <esharri2@gmail.com>
2020-04-26 12:45:17 -0400

    netlify id fix

--- js/admin/index.js
+++ js/admin/index.js
@@ -1,6 +1,7 @@
 const pathname = new URL(window.location.href).pathname;
 
 if (pathname.includes("/admin") || pathname.includes("/#")) {
+  console.log("loading Netlify Identity...");
   (async () => {
     try {
       await import("./loadNetlifyIdentity.js");
