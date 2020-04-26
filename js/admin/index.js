ce104a96a6151fc0aadb50a3748cdd61d0853a95
Evan Harrison <esharri2@gmail.com>
2020-04-26 14:10:36 -0400

    custom img cmp

--- js/admin/index.js
+++ js/admin/index.js
@@ -2,7 +2,6 @@ const url = new URL(window.location.href)
 const pathname = url.pathname;
 
 if (url.pathname.includes("/admin") || url.hash.includes("recovery_token")) {
-  console.log("loading Netlify Identity...");
   (async () => {
     try {
       await import("./loadNetlifyIdentity.js");
