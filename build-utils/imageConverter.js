60a121732e0d7ec52b111de90d219ede9881bf0c
Evan Harrison <esharri2@gmail.com>
2020-05-09 11:40:18 -0400

    update image converter

--- build-utils/imageConverter.js
+++ build-utils/imageConverter.js
@@ -1,10 +1,7 @@
 const sharp = require("sharp");
 const path = require("path");
 const glob = require("glob");
-const fs = require("fs");
 const del = require("del");
-
-// TODO make it so an image does not get stretched above native size
 
 const mediaDir = "./dist/media/source/*.*";
 const sizes = [
@@ -13,33 +10,40 @@ const sizes = [
   { tag: "sm", size: 480 },
 ];
 
+// Git list of images
 const getImageNames = () => glob.sync(mediaDir);
 
-const createImages = async (image) => {
+// Given an image, create a rendition for each size
+const createImages = (image) => {
   const name = path.parse(image).name;
   const ext = path.extname(image);
 
-  sizes.forEach((size) => {
-    // if native size of image is above
+  const promises = sizes.map((size) => {
     sharp(image)
       .resize({ width: size.size, withoutEnlargement: true })
       .toFile(`./dist/media/${name}-${size.tag}${ext}`)
       .then(() => {
         return true;
-      });
+      })
+      .catch((error) => console.error(error));
   });
+
+  Promise.all(promises).then((values) => values);
 };
 
 const init = () => {
   const imageNames = getImageNames();
-  imageNames.forEach((image) => {
-    createImages(image);
-  });
 
-  (async () => {
-    const deletePaths = await del(["dist/media/source/*"]);
-    console.log(`${deletePaths.length} images have been deleted.`);
-  })();
+  Promise.all(
+    imageNames.map((img) => {
+      createImages(img);
+    })
+  )
+    .then(async (values) => {
+      const deletePaths = await del(["dist/media/source/*"]);
+      console.log(`${deletePaths.length} images have been deleted.`);
+    })
+    .catch((error) => console.error(error));
 };
 
 init();
