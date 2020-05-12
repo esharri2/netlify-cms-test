81665856989bd2ebab77545837924470ad80e1c1
Evan Harrison <esharri2@gmail.com>
2020-05-11 20:56:21 -0400

    fix netlify id

--- js/admin/index.js
+++ js/admin/index.js
@@ -1,7 +1,11 @@
-const url = new URL(window.location.href)
+const url = new URL(window.location.href);
 const pathname = url.pathname;
 
-if (url.pathname.includes("/admin") || url.hash.includes("recovery_token")) {
+if (
+  url.pathname.includes("/admin") ||
+  url.hash.includes("recovery_token") ||
+  url.hash.includes("invite_token")
+) {
   (async () => {
     try {
       await import("./loadNetlifyIdentity.js");
